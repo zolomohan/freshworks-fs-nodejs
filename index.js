b9175f36757b6924f0513e86ba17b98bc05c15a8
zolomohan <mohanzolomon@gmail.com>
2021-01-01 20:17:46 +0530

    Add Comments

--- index.js
+++ index.js
@@ -10,26 +10,39 @@ const rl = readline.createInterface({
 let lockFileLocation = null;
 
 rl.question("Enter File Location (Leave it empty to create in D:/fs-files): ", (fileLocation) => {
+  // If File Location is Not Given,
   if (!fileLocation) {
+    // Set File Location
     fileLocation = `D:/fs-files/${uuidv4()}.txt`;
+
+    // If fs-files directory doesn't exist, create the directory.
     if (!fs.existsSync("D:/fs-files")) fs.mkdirSync("D:/fs-files");
+
+    // Create the new file.
     createFile(fileLocation);
   }
 
+  if (!fs.existsSync(fileLocation)) createFile(fileLocation);
+
+  // Check for File size, If its more than 1GB, Stop the process.
   let stats = fs.statSync(fileLocation);
   if (stats.size > 1024 * 1000000) {
     console.log("File Size is Greater than 1GB");
     rl.close();
   }
 
+  // Generate Lock File Location, by removing file extention and adding the .lock extention to the name,
   lockFileLocation = fileLocation.split(".")[0] + ".lock";
 
+  // Check for a lock file, if exists, stop process.
   if (fs.existsSync(lockFileLocation)) {
     console.log("This file is being used by another process.");
     rl.close();
     return;
   }
 
+  // This part of the code will only be reached if the file can be accessed.
+  // Create a lock file to prevent other processes to access this file.
   createFile(lockFileLocation);
 
   rl.question("1. Create\n2. Read\n3. Delete\n\nEnter Option: ", (option) => {
@@ -37,32 +50,40 @@ rl.question("Enter File Location (Leave it empty to create in D:/fs-files): ", (
 
     if (option === 1) {
       rl.question("Enter Key: ", (key) => {
+        // Check Key Size, Stop process if greater than 32
         if (key.length > 32) {
           console.log("Key Must be below 32 Characters");
           closeAccess();
         }
 
         rl.question("Enter Value: ", (value) => {
-          if (value[0] === "{" && value[value.length - 1] === "}") {
-            value = JSON.parse(value);
-          }
-
+          // Check value byte size, if greater than 16KB, stop process.
           if (value.length * 2 > 16000) {
             console.log("Value Must be less than 16KB");
             closeAccess();
           }
 
+          // Check if the value given is an JSON object. If yes, parse the string.
+          if (value[0] === "{" && value[value.length - 1] === "}") {
+            value = JSON.parse(value);
+          }
+
           rl.question("Enter Time to Live in Seconds: (Optional) ", (time) => {
-            time = time === "" ? null : Date.now() + time * 1000;
+            // If time to live is given, get current timestamp and the seconds to it.
+            time = !time ? null : Date.now() + time * 1000;
 
             fs.readFile(fileLocation, "utf8", (err, data) => {
               let writeData = {};
-              if (data === undefined || data === "") {
+
+              // If file empty or non-existent
+              if (data === undefined || !data) {
                 writeData[key] = { timeToLive: time, value };
                 fs.writeFile(fileLocation, JSON.stringify(writeData), closeAccess);
                 return;
               } else {
                 writeData = JSON.parse(data);
+
+                // If the key already exits, don't overwrite it.
                 if (writeData[key] !== undefined) {
                   console.log("This key already exists.");
                   closeAccess();
@@ -83,6 +104,7 @@ rl.question("Enter File Location (Leave it empty to create in D:/fs-files): ", (
           if (data[key] === undefined) {
             console.log("This key doesn't exist.");
           } else if (data[key].timeToLive < Date.now()) {
+            // Check current timestamp with Time to Live
             console.log("You can't access this value anymore. It has expired");
           } else {
             console.log("Value:", data[key].value);
@@ -98,6 +120,7 @@ rl.question("Enter File Location (Leave it empty to create in D:/fs-files): ", (
           if (data[key] === undefined) {
             console.log("This key doesn't exists.");
           } else if (data[key].timeToLive < Date.now()) {
+            // Check current timestamp with Time to Live
             console.log("You can't access this value anymore. It has expired");
           } else {
             delete data[key];
@@ -113,11 +136,13 @@ rl.question("Enter File Location (Leave it empty to create in D:/fs-files): ", (
   });
 });
 
+// Function to create a file.
 function createFile(location) {
   let createStream = fs.createWriteStream(location);
   createStream.end();
 }
 
+// Function to delete LOCK file and close readline stream.
 function closeAccess() {
   fs.unlinkSync(lockFileLocation);
   rl.close();
